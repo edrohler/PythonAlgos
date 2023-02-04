f6335a3d5f934c953fe0989316397cb8ba136d08
Eric D. Rohler <development@eric.rohler>
2023-02-03 19:49:53 -0500

    Add Factorials Exercise

---
+++ 01-Lesson/Factorials/main.py
@@ -0,0 +1,17 @@
+def iterative_factorial(n):
+    fact = 1
+    for i in range(2, n+1):
+        fact *=i
+    return fact
+
+print(iterative_factorial(5))
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+            temp = recur_factorial(n -1)
+            return temp * n
+
+print (recur_factorial(10))
+    
